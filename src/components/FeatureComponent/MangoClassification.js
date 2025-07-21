ed127430b0f9f32f30ee48dd56ca320f3cd21fc3
TienPham39 <tienpg39@gmail.com>
2025-07-21 14:43:29 +0700

    Add predicted mango

---
+++ src/components/FeatureComponent/MangoClassification.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MangoClassification = () => {
+  return (
+    <div className="w-full h-[85vh]">
+      <iframe
+        src="https://mangovate-fe.vercel.app/"
+        title="Phân loại xoài"
+        width="100%"
+        height="100%"
+        style={{ border: "none" }}
+      />
+    </div>
+  );
+};
+
+export default MangoClassification;
